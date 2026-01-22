a9cf0b525542f8edc81ede59e76ef29cbbb1646d
Justin Crawford <bikerhiker10@gmail.com>
2026-01-22 12:02:11 -0500

    feat(11-02): wire OfflineProvider and connection UI into app
    - Import OfflineProvider from contexts/OfflineContext
    - Import ConnectionToast and OfflineIndicator components
    - Wrap children with OfflineProvider inside ThemeProvider
    - Add ConnectionToast and OfflineIndicator components globally
    - Provider order: SessionProvider > ThemeProvider > OfflineProvider > children

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -2,11 +2,20 @@
 
 import { SessionProvider } from 'next-auth/react'
 import { ThemeProvider } from '@/contexts/ThemeContext'
+import { OfflineProvider } from '@/contexts/OfflineContext'
+import { ConnectionToast } from '@/components/ConnectionToast'
+import { OfflineIndicator } from '@/components/OfflineIndicator'
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
     <SessionProvider>
-      <ThemeProvider>{children}</ThemeProvider>
+      <ThemeProvider>
+        <OfflineProvider>
+          {children}
+          <ConnectionToast />
+          <OfflineIndicator />
+        </OfflineProvider>
+      </ThemeProvider>
     </SessionProvider>
   )
 }
